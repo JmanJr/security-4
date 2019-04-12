9e4a3a2a522f867fcc695363c599e511ef157eb8
kunseok <bryanklee@utexas.edu>
2019-04-12 13:08:21 -0500

    new elf

--- ch3_bof/greetings.c
+++ ch3_bof/greetings.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 void secret() {
-  gets("Well, that seems about right!\n");
+  puts("Well, that seems about right!\n");
   system("/bin/bash");
 }
 
 void hello() {
-  gets("Hello, Bilbo.\n");
-  gets("I am looking for someone to share in an adventure that I\n");
-  gets("am arranging, and it's very difficult to find anyone.\n");
-  gets("I suppose you will have to join me. Any idea where we're\n");
-  gets("going?\n");
+  puts("Hello, Bilbo.\n");
+  puts("I am looking for someone to share in an adventure that I\n");
+  puts("am arranging, and it's very difficult to find anyone.\n");
+  puts("I suppose you will have to join me. Any idea where we're\n");
+  puts("going?\n");
   char dest[20];
   gets(dest);
 }
