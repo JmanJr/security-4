19e8c1cdbe999cdeb7f1df38211d26f4106f97fc
Ryan Morris <ryanmorris708@gmail.com>
2019-04-13 17:38:06 -0500

    smaug's lair MIGHT work, need to try to solve

--- ch5_wip/stealtreasure.js
+++ ch5_wip/stealtreasure.js
@@ -1,11 +1,22 @@
+var treasureCount = 45290;
+
+function initMission() {
+	console.log("hello");
+	document.getElementById("mission").style.display = "block";
+	document.getElementById("treasureCount").innerHTML = treasureCount;
+	console.log("goodbye");
+}
+
 function steal(num) {
-	if ((var treasure_count = document.getElementById("treasure_count")) > 0) {
-		document.getElementById("treasure_count").innerhtml = treasure_count - num;
+	if ((treasureCount - num) > 0) {
+		treasureCount = treasureCount - num;
+		document.getElementById("treasureCount").innerHTML = treasureCount;
 	} else {
+		document.getElementById("treasureCount").innerHTML = 0;
 		var flagReveal = document.createElement("div");
 		var message = document.createTextNode("You successfully stole all the treasure! The flag for this challenge is lotrflag{best_robber_ever}.");
 		flagReveal.appendChild(message);
 		var body = document.getElementsByTagName("body")[0];
-		document.body.insert(flagReveal);
+		document.body.appendChild(flagReveal);
 	}
 }
